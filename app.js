ec747840a3bc0cd57999a4639c2938b729eb2410
dezhizhang <1541609448@qq.com>
2019-04-11 22:07:44 +0800

    基本服务

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict'
-const http = require('http');
+ const http = require('http');
 
-let app = http.createServer((req,res) => {
+ let app = http.createServer((req,res) => {
+     res.writeHead(200,{'Content-Type':'text/html'});
+     res.end('index')
+ });
 
-});
+ app.listen(8082,() => {
+     console.log('server is run ')
+ })
+ 
 
-app.listen(8082,() => {
-    console.log('server is run ')
-})
 
