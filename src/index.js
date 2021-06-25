69fd7abb79f00cfe5c3513ebc577dc18a31d1676
dezhizhang <1541609448@qq.com>
2021-06-25 21:15:20 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 /*
  * :file description: 
  * :name: /webRTC/src/index.js
@@ -5,5 +6,11 @@
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-06-25 20:35:28
  * :last editor: 张德志
- * :date last edited: 2021-06-25 20:35:28
+ * :date last edited: 2021-06-25 21:13:35
  */
+(async function(){
+    const video = document.querySelector('video');
+    video.srcObject = await navigator.mediaDevices.getUserMedia({
+        video:true
+    })
+})()
