ccd727d0490bc05ca14cd36574a6be82adb46e2f
dezhizhang <1541609448@qq.com>
2021-06-26 08:40:29 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@
  * :copyright: (c) 2021, Tungee
  * :date created: 2021-06-25 20:35:28
  * :last editor: 张德志
- * :date last edited: 2021-06-25 21:13:35
+ * :date last edited: 2021-06-26 08:38:58
  */
-(async function(){
-    const video = document.querySelector('video');
-    video.srcObject = await navigator.mediaDevices.getUserMedia({
-        video:true
-    })
-})()
+// (async function(){
+//     const video = document.querySelector('video');
+//     video.srcObject = await navigator.mediaDevices.getUserMedia({
+//         video:true
+//     })
+// })()
+(async function() {
+    // let devices = await navigator.mediaDevices.enumerateDevices();
+    // console.log(devices);
+    let strem = await navigator.mediaDevices.getUserMedia({video:false,audio:true});
+    document.querySelector("audio").srcObject = strem;
+})();
